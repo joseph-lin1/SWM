f9763353fcefab16694d8dc83c4d19f8a25ae13e
Joseph Lin <Joseph Lin>
2017-07-15 09:52:21 -0700

    add

---
+++ public/js/chat.js
@@ -0,0 +1,399 @@
+var topics = ["What was the last funny video you saw?",
+"What do you do to get rid of stress?",
+"What is something you are obsessed with?",
+"Who is your favorite entertainer (comedian, musician, actor, etc.)?",
+"What’s your favorite way to waste time?",
+"Do you have any pets? What are their names?",
+"Where did you go last weekend? What did you do?",
+"What are you going to do this weekend?",
+"What is something that is popular now that annoys you?",
+"What did you do on your last vacation?",
+"What was the last time you worked incredibly hard?",
+"Are you very active of do you prefer to just relax in your free time?",
+"What do you do when you hang out with your friends?",
+"Who is your oldest friend? Where did you meet them?",
+"What’s the best/worst thing about your work/school?",
+"If you had intro music, what song would it be? Why?",
+"What were you really into when you were a kid?",
+"What three words best describe you?",
+"What would be your perfect weekend?",
+"What do you think of tattoos? Do you have any?",
+"What’s your favorite number? Why?",
+"What’s the most useful thing you own?",
+"Have you ever saved an animal’s life? How about a person’s life?",
+"If you opened a business, what kind of business would it be?",
+"Are you a very organized person?",
+"Have you ever spoke in front of a large group of people? How did it go?",
+"What is the strangest dream you have ever had?",
+"What is a controversial opinion you have?",
+"Who in your life brings you the most joy?",
+"Who had the biggest impact on the person you have become?",
+"What is the most annoying habit someone can have?",
+"Where is the most beautiful place you have been?",
+"Where do you spend most of your free time / day?",
+"Who was your best friend in elementary school?",
+"How often do you stay up past 3 a.m.?",
+"What’s your favorite season? Why?",
+"Which is more important, a great car or a great house? Why?",
+"What animal or insect do you wish humans could eradicate?",
+"Where is the most beautiful place near where you live?",
+"What do you bring with you everywhere you go?",
+"How much time do you spend on the internet? What do you usually do?",
+"What is the most disgusting habit some people have?",
+"Where and when was the most amazing sunset you have ever seen?",
+"Do you like horror movies? Why or why not?",
+"What song always puts you in a good mood?",
+"Which do you prefer, popular music or relatively unknown music?",
+"What was the last song you listened to?",
+"What is your favorite movie soundtrack?",
+"Are there any songs that always bring a tear to your eye?",
+"Do you like going to concerts?",
+"Who was the first band or musician you were really into?",
+"Records, tapes, CDs, MP3s. Which did you grow up with?",
+"What bands or types of music do you listen to when you exercise?",
+"Do you like classical music?",
+"What’s the best way to discover new music?"
+];
+
+var chosen = topics[Math.round((Math.random() * 55))];
+console.log(chosen);
+
+$("#topic-name").text(chosen);
+
+// This file is executed in the browser, when people visit /chat/<random id>
+
+$(function(){
+
+	// getting the id of the room from the url
+	var id = Number(window.location.pathname.match(/\/chat\/(\d+)$/)[1]);
+
+	// connect to the socket
+	var socket = io();
+	
+	// variables which hold the data for each person
+	var name = "",
+		email = "",
+		img = "",
+		friend = "";
+
+	// cache some jQuery objects
+	var section = $(".section"),
+		footer = $("footer"),
+		onConnect = $(".connected"),
+		inviteSomebody = $(".invite-textfield"),
+		personInside = $(".personinside"),
+		chatScreen = $(".chatscreen"),
+		left = $(".left"),
+		noMessages = $(".nomessages"),
+		tooManyPeople = $(".toomanypeople");
+
+	// some more jquery objects
+	var chatNickname = $(".nickname-chat"),
+		leftNickname = $(".nickname-left"),
+		loginForm = $(".loginForm"),
+		yourName = $("#yourName"),
+		yourEmail = $("#yourEmail"),
+		hisName = $("#hisName"),
+		hisEmail = $("#hisEmail"),
+		chatForm = $("#chatform"),
+		textarea = $("#message"),
+		messageTimeSent = $(".timesent"),
+		chats = $(".chats");
+
+	// these variables hold images
+	var ownerImage = $("#ownerImage"),
+		leftImage = $("#leftImage"),
+		noMessagesImage = $("#noMessagesImage");
+
+
+	// on connection to server get the id of person's room
+	socket.on('connect', function(){
+
+		socket.emit('load', id);
+	});
+
+	// save the gravatar url
+	socket.on('img', function(data){
+		img = data;
+	});
+
+	// receive the names and avatars of all people in the chat room
+	socket.on('peopleinchat', function(data){
+
+		if(data.number === 0){
+
+			showMessage("connected");
+
+			loginForm.on('submit', function(e){
+
+				e.preventDefault();
+
+				name = $.trim(yourName.val());
+				
+				if(name.length < 1){
+					alert("Please enter a nick name longer than 1 character!");
+					return;
+				}
+
+				email = yourEmail.val();
+
+				if(!isValid(email)) {
+					alert("Please enter a valid email!");
+				}
+				else {
+
+					showMessage("inviteSomebody");
+
+					// call the server-side function 'login' and send user's parameters
+					socket.emit('login', {user: name, avatar: email, id: id});
+				}
+			
+			});
+		}
+
+		else if(data.number === 1) {
+
+			showMessage("personinchat",data);
+
+			loginForm.on('submit', function(e){
+
+				e.preventDefault();
+
+				name = $.trim(hisName.val());
+
+				if(name.length < 1){
+					alert("Please enter a nick name longer than 1 character!");
+					return;
+				}
+
+				if(name == data.user){
+					alert("There already is a \"" + name + "\" in this room!");
+					return;
+				}
+				email = hisEmail.val();
+
+				if(!isValid(email)){
+					alert("Wrong e-mail format!");
+				}
+				else {
+					socket.emit('login', {user: name, avatar: email, id: id});
+				}
+
+			});
+		}
+
+		else {
+			showMessage("tooManyPeople");
+		}
+
+	});
+
+	// Other useful 
+
+	socket.on('startChat', function(data){
+		console.log(data);
+		if(data.boolean && data.id == id) {
+
+			chats.empty();
+
+			if(name === data.users[0]) {
+
+				showMessage("youStartedChatWithNoMessages",data);
+			}
+			else {
+
+				showMessage("heStartedChatWithNoMessages",data);
+			}
+
+			chatNickname.text(friend);
+		}
+	});
+
+	socket.on('leave',function(data){
+
+		if(data.boolean && id==data.room){
+
+			showMessage("somebodyLeft", data);
+			chats.empty();
+		}
+
+	});
+
+	socket.on('tooMany', function(data){
+
+		if(data.boolean && name.length === 0) {
+
+			showMessage('tooManyPeople');
+		}
+	});
+
+	socket.on('receive', function(data){
+
+		showMessage('chatStarted');
+
+		if(data.msg.trim().length) {
+			createChatMessage(data.msg, data.user, data.img, moment());
+			scrollToBottom();
+		}
+	});
+
+	textarea.keypress(function(e){
+
+		// Submit the form on enter
+
+		if(e.which == 13) {
+			e.preventDefault();
+			chatForm.trigger('submit');
+		}
+
+	});
+
+	chatForm.on('submit', function(e){
+
+		e.preventDefault();
+
+		// Create a new chat message and display it directly
+
+		showMessage("chatStarted");
+
+		if(textarea.val().trim().length) {
+			createChatMessage(textarea.val(), name, img, moment());
+			scrollToBottom();
+
+			// Send the message to the other person in the chat
+			socket.emit('msg', {msg: textarea.val(), user: name, img: img});
+
+		}
+		// Empty the textarea
+		textarea.val("");
+	});
+
+	// Update the relative time stamps on the chat messages every minute
+
+	setInterval(function(){
+
+		messageTimeSent.each(function(){
+			var each = moment($(this).data('time'));
+			$(this).text(each.fromNow());
+		});
+
+	},60000);
+
+	// Function that creates a new chat message
+
+	function createChatMessage(msg,user,imgg,now){
+
+		var who = '';
+
+		if(user===name) {
+			who = 'me';
+		}
+		else {
+			who = 'you';
+		}
+
+		var li = $(
+			'<li class=' + who + '>'+
+				'<div class="image">' +
+					'<img src=' + imgg + ' />' +
+					'<b></b>' +
+					'<i class="timesent" data-time=' + now + '></i> ' +
+				'</div>' +
+				'<p></p>' +
+			'</li>');
+
+		// use the 'text' method to escape malicious user input
+		li.find('p').text(msg);
+		li.find('b').text(user);
+
+		chats.append(li);
+
+		messageTimeSent = $(".timesent");
+		messageTimeSent.last().text(now.fromNow());
+	}
+
+	function scrollToBottom(){
+		$("html, body").animate({ scrollTop: $(document).height()-$(window).height() },1000);
+	}
+
+	function isValid(thatemail) {
+
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(thatemail);
+	}
+
+	function showMessage(status,data){
+
+		if(status === "connected"){
+
+			section.children().css('display', 'none');
+			onConnect.fadeIn(1200);
+		}
+
+		else if(status === "inviteSomebody"){
+
+			// Set the invite link content
+			$("#link").text(window.location.href);
+
+			onConnect.fadeOut(1200, function(){
+				inviteSomebody.fadeIn(1200);
+			});
+		}
+
+		else if(status === "personinchat"){
+
+			onConnect.css("display", "none");
+			personInside.fadeIn(1200);
+
+			chatNickname.text(data.user);
+			ownerImage.attr("src",data.avatar);
+		}
+
+		else if(status === "youStartedChatWithNoMessages") {
+
+			left.fadeOut(1200, function() {
+				inviteSomebody.fadeOut(1200,function(){
+					noMessages.fadeIn(1200);
+					footer.fadeIn(1200);
+				});
+			});
+
+			friend = data.users[1];
+			noMessagesImage.attr("src",data.avatars[1]);
+		}
+
+		else if(status === "heStartedChatWithNoMessages") {
+
+			personInside.fadeOut(1200,function(){
+				noMessages.fadeIn(1200);
+				footer.fadeIn(1200);
+			});
+
+			friend = data.users[0];
+			noMessagesImage.attr("src",data.avatars[0]);
+		}
+
+		else if(status === "chatStarted"){
+
+			section.children().css('display','none');
+			chatScreen.css('display','block');
+		}
+
+		else if(status === "somebodyLeft"){
+
+			leftImage.attr("src",data.avatar);
+			leftNickname.text(data.user);
+
+			section.children().css('display','none');
+			footer.css('display', 'none');
+			left.fadeIn(1200);
+		}
+
+		else if(status === "tooManyPeople") {
+
+			section.children().css('display', 'none');
+			tooManyPeople.fadeIn(1200);
+		}
+	}
+
+});
